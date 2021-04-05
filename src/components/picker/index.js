a92c53284b1c5179a268ffa954e9bc456ecdec45
Yasin Efem  Dalkılıç <yefem.dalkilic@gmail.com>
2021-04-05 19:47:21 +0300

    added picker loading

--- src/components/picker/index.js
+++ src/components/picker/index.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
-import { StyleSheet } from 'react-native'
+import React, { useState, useEffect, useMemo } from 'react'
+import { StyleSheet, ActivityIndicator } from 'react-native'
 import RNPickerSelect from 'react-native-picker-select';
 import { Box } from '../../components'
 import Consts from '../../consts'
+import axios from 'axios'
+import { colors } from '../../theme';
 
 //seçili olana renk
 // { label: 'Orange', value: 'orange', color: colors.primary },
@@ -14,18 +16,42 @@ import Consts from '../../consts'
                 { label: 'Hockey', value: 'hockey', color: null },
             ]
 */
-const Picker = ({ items = [], placeholder = {}, defaultValue = {}, selectedValue }) => {
-    const [defaultValue, setDefaultValue] = useState(defaultValue)
+const Picker = ({ placeholder = {}, defaultVal = {}, selectedValue, dataSourceUrl }) => {
+    const [defaultValue, setDefaultValue] = useState(defaultVal);
+    const [dataSourceLoading, setDataSourceLoading] = useState(false);
+    const [items, setItems] = useState([]);
     const setSelectedItem = (value) => {
         setDefaultValue(value)
         //selectedValue(value)
+    };
+    useEffect(() => {
+        setDataSourceLoading(true)
+        fetchOptionsList();
+    }, []);
+
+    const fetchOptionsList = async () => {
+        const { data } = await axios.get('https://morning-garden-20509.herokuapp.com/api/yasel/options')
+        setDataSourceLoading(false)
+        setItems(data)
     }
+
+    const renderCustomIcon = useMemo(() => {
+        console.log('loadmı', dataSourceLoading)
+        if (dataSourceLoading) {
+            return <ActivityIndicator size="small" color={colors.primary} />
+        }
+        return <Box size='20' bg='red' borderRadius={9999} alignItems='center' justifyContent='center' />
+    }, [dataSourceLoading])
+
+
+
     return (
         <RNPickerSelect
+            disabled={dataSourceLoading}
             onValueChange={(value) => { setSelectedItem(value) }}
             value={defaultValue}
             placeholder={placeholder}
-            Icon={() => <Box size='20' bg='red' borderRadius={9999} alignItems='center' justifyContent='center' />}
+            Icon={() => renderCustomIcon}
             useNativeAndroidPickerStyle={false}
             style={{
                 ...pickerSelectStyles, iconContainer: {
--- src/screens/newItemModal/index.js
+++ src/screens/newItemModal/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Input, Box, Picker, FocusStatusBar } from '../../components'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { colors, space, radius, font, sizes, border } from '../../theme'
 const ModalScreen = ({ navigation, children }) => {
+
     return (
         <SafeAreaView style={{ backgroundColor: colors.pageBg, flex: 1, position: 'relative' }}>
             <FocusStatusBar barStyle="dark-content" backgroundColor={colors.pageBg} />
@@ -10,7 +11,13 @@ const ModalScreen = ({ navigation, children }) => {
                 <Input mb={space.mb20} bg={colors.white} width='100%' borderRadius={radius.bsmall} fontSize={font.size14} height={sizes.height56}
                     borderColor={colors.borderColor}
                     pl={space.pl16} py={space.pv17} border={border.xsmall} color={colors.inputText} />
-                <Picker />
+                <Picker dataSourceUrl='' items={[
+                    { label: 'Football', value: 'football', color: null },
+                    { label: 'Önemli', value: 'orange', color: null, },
+                    { label: 'Football', value: 'football', color: null },
+                    { label: 'Baseball', value: 'baseball', color: null },
+                    { label: 'Hockey', value: 'hockey', color: null },
+                ]} />
             </Box>
         </SafeAreaView>
     );
